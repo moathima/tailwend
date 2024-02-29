8877298068d0d3b1cb21222d1845b8d3d306863a
moathima <moathima86@gmail.com>
2024-02-29 18:29:00 +0300

    vv

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./"],
+  theme: {
+    extend: {
+      colors: {
+        "buttons":'#b88e2f',
+        "pink":"#fcf8f3"
+      },
+
+      width:{
+        "button-1":"176px",
+        "368;":"368px",
+        "200":"200px",
+        "285":"285px",
+        "200":"200px"
+
+
+      }, 
+
+      height:{
+        "48px":"48px",
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
